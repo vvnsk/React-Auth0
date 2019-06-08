42e29fee51c22e507762b0771812184651c9be8a
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-06-08 17:22:14 -0500

    React Context

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "./AuthContext";
 
 class Home extends Component {
   render() {
-    const { isAuthenticated, login } = this.props.auth;
     return (
-      <div>
-        <h1>Home</h1>
-        {isAuthenticated() ? (
-          <Link to="/profile">View Profile</Link>
-        ) : (
-          <button onClick={login}>Login</button>
+      <AuthContext.Consumer>
+        {auth => (
+          <div>
+            <h1>Home</h1>
+            {auth.isAuthenticated() ? (
+              <Link to="/profile">View Profile</Link>
+            ) : (
+              <button onClick={auth.login}>Login</button>
+            )}
+          </div>
         )}
-      </div>
+      </AuthContext.Consumer>
     );
   }
 }
